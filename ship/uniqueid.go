ef68aab013426da07bc1350b0be64cd8f4bbc497
Andreas Linde <mail@andreaslinde.de>
2022-09-28 20:33:24 +0200

    Add method for providing machineID externally
    This helps evcc as it has an additional way to provide a machineID via its configuration file

--- ship/uniqueid.go
+++ ship/uniqueid.go
@@ -1,6 +1,7 @@
 package ship
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/panta/machineid"
@@ -14,4 +15,15 @@ func UniqueID(prefix, appID string) (string, error) {
 	}
 
 	return fmt.Sprintf("%s-%0x", prefix, s[:8]), nil
+}
+
+// UniqueID creates ship ID with given prefix and salted by provided protectedID
+// The protectedID is basically the same as `machineid.ProtectedID(appId)` above
+// but provided by the system using this library
+func UniqueIDWithProtectedID(prefix, protectedID string) (string, error) {
+	if len(protectedID) == 0 {
+		return "", errors.New("A generated machine specific protectedID needs to be provided")
+	}
+
+	return fmt.Sprintf("%s-%0x", prefix, protectedID[:8]), nil
 }
