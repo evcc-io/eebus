f635b19611c85e09221684ef44a085dc1946b9f2
Andreas Linde <mail@andreaslinde.de>
2022-09-29 16:00:44 +0200

    Remove old UniqueID generation

--- go.mod
+++ go.mod
@@ -7,13 +7,11 @@ require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/libp2p/zeroconf/v2 v2.2.0
 	github.com/mitchellh/mapstructure v1.4.1
-	github.com/panta/machineid v1.0.1
 	github.com/rickb777/date v1.17.0
 	github.com/samber/lo v1.21.0
 )
 
 require (
-	github.com/denisbrodbeck/machineid v1.0.1 // indirect
 	github.com/miekg/dns v1.1.43 // indirect
 	github.com/rickb777/plural v1.4.1 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
--- ship/uniqueid.go
+++ ship/uniqueid.go
@@ -3,19 +3,7 @@ package ship
 import (
 	"errors"
 	"fmt"
-
-	"github.com/panta/machineid"
 )
-
-// UniqueID creates ship ID with given prefix and salted by appID
-func UniqueID(prefix, appID string) (string, error) {
-	s, err := machineid.ProtectedID(appID)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%s-%0x", prefix, s[:8]), nil
-}
 
 // UniqueID creates ship ID with given prefix and salted by provided protectedID
 // The protectedID is basically the same as `machineid.ProtectedID(appId)` above
